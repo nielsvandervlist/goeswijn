15d11469e9086d8bd61e4ccda171cf06155530d9
Niels <niels.vder.vlist@gmail.com>
2023-08-02 21:04:53 +0200

    test

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,4 @@
 export const EXAMPLE_PATH = 'cms-wordpress'
 export const CMS_NAME = 'WordPress'
 export const CMS_URL = 'https://wordpress.org'
-export const HOME_OG_IMAGE_URL =
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV7nsMTXaLKRuhAcmikEsDdZIf3Pso6yfovis6_9muumu04sIyFO64rzgTFZMpg-VsaCQ&usqp=CAU'
+export const HOME_OG_IMAGE_URL = 'favicon/android-chrome-144x144.png'
