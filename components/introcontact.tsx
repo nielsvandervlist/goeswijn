c1dc8abeb5f8d9d82be22b2f92bc9e47f77149f5
Niels <niels.vder.vlist@gmail.com>
2023-08-02 20:48:41 +0200

    add location

--- components/introcontact.tsx
+++ components/introcontact.tsx
@@ -1,21 +1,35 @@
 import {CMS_NAME, CMS_URL} from '../lib/constants'
 import Blocks from "./blocks";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faArrowDown, faLocationDot} from "@fortawesome/free-solid-svg-icons";
+import React from "react";
 
 export default function IntroContact() {
     return (
         <div className={'overflow-hidden'}>
             <section
                 className={'text-center container overflow-hidden flex items-center justify-center min-h-screen flex-col'}>
-                <div className={'-mt-[150px]'}>
+                <div className={'py-40 text-center flex flex-col'}>
                     <h1 className={'text-orange text-[54px] lg:text-[120px] tracking-widest'}>Contact</h1>
                     <h2 className={'text-brown underline-yellow intro text-[28px] lg:text-[32px] tracking-widest'}>open 12-laat
                         dinsdagen dicht</h2>
-                    <ul className={'text tracking-widest mt-20'}>
-                        <li className={'text-[18px] lg:text-[24px] mb-4'}>Choorlammersteeg 1D</li>
-                        <li className={'text-[18px] lg:text-[24px] mb-4'}>2311 EP leiden</li>
-                        <li className={'text-[18px] lg:text-[24px] mb-4'}>071-785 78 10</li>
-                        <li className={'text-[18px] lg:text-[24px] mb-4'}>bistrogoeswijn@gmail.com</li>
-                    </ul>
+                    <div className={'pb-20 lg:pt-10 pt-14 flex gap-20 items-center justify-center relative'}>
+                        <ul className={'text tracking-widest mx-auto text-center w-full flex flex-col gap-4'}>
+                            <li className={'text-[24px] lg:text-[32px] mb-4 written'}>Choorlammersteeg 1D</li>
+                            <li className={'text-[24px] lg:text-[32px] mb-4 written'}>2311 EP leiden</li>
+                            <li className={'text-[24px] lg:text-[32px] mb-4 written'}>
+                                <a href={"tel:071-785-78-10"}>071-785 78 10</a>
+                            </li>
+                            <li className={'text-[24px] lg:text-[32px] mb-4 written'}>bistrogoeswijn@gmail.com</li>
+                            <li>
+                                <a target={'_blank'} href={'https://www.google.com/maps/place/Bistro+Goeswijn/@52.157204,4.4894487,17z/data=!3m1!4b1!4m6!3m5!1s0x47c5c7fda83cdef3:0x63df6ddbf3daefd8!8m2!3d52.157204!4d4.4920236!16s%2Fg%2F11sj59fm7j?entry=ttu'}>
+                                    <h3 className={'route z-20 text-[32px] lg:text-[48px] text-brown p-4 w-full block mb-10'}>
+                                        <FontAwesomeIcon icon={faLocationDot}/> Route</h3>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
                 </div>
             </section>
         </div>
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { GetStaticProps } from 'next'
+import {GetStaticProps} from 'next'
 import Container from '../components/container'
 import Layout from '../components/layout'
 import {getMenuContent} from '../lib/api'
@@ -8,27 +8,32 @@ import IntroContact from "../components/introcontact";
 import Blocks from "../components/blocks";
 import PageTransition from "../components/PageTransition";
 import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faLocationDot} from "@fortawesome/free-solid-svg-icons";
 
 type IndexPageProps = {}
 type IndexPageRef = React.ForwardedRef<HTMLDivElement>
 
-export default function Menu({ menu }, preview, props: IndexPageProps, ref: IndexPageRef ) {
+export default function Menu({menu}, preview, props: IndexPageProps, ref: IndexPageRef) {
 
-  return (
-      <PageTransition ref={ref}>
-    <Layout preview={preview}>
-      <Head>
-        <title>{`Bistro Goeswijn`}</title>
-      </Head>
-      <Container>
-        <IntroContact />
-      </Container>
-        <div className={'relative overflow-x-hidden w-full h-[180px] -mt-[180px]'}>
-            <div className={'absolute bottom-[1px] -left-[60px] right-0'}><Blocks className={'bg-orange'}/></div>
-            <div className={'absolute bottom-[59px] left-0 right-0'}><Blocks className={'bg-orange'}/></div>
-            <div className={'absolute bottom-[117px] -left-[60px] right-0'}><Blocks className={'bg-orange'}/></div>
-        </div>
-    </Layout>
-      </PageTransition>
-  )
+    return (
+        <PageTransition ref={ref}>
+            <Layout preview={preview}>
+                <Head>
+                    <title>{`Bistro Goeswijn`}</title>
+                </Head>
+                <Container>
+                    <IntroContact/>
+                </Container>
+                <div className={'relative overflow-x-hidden w-full h-[180px] -mt-[180px]'}>
+                    <div className={'absolute bottom-[1px] -left-[60px] right-0'}><Blocks className={'bg-orange'}/>
+                    </div>
+                    <div className={'absolute bottom-[59px] left-0 right-0'}><Blocks className={'bg-orange'}/></div>
+                    <div className={'absolute bottom-[117px] -left-[60px] right-0'}><Blocks className={'bg-orange'}/>
+                    </div>
+                </div>
+
+            </Layout>
+        </PageTransition>
+    )
 }
