7bf9e348a31e06571bfe1c7d67f5afe07cf198f3
Niels van der Vlist <nielsvandervlist@leiderdorpinstruments.nl>
2024-07-23 09:46:38 +0200

    Edit teskt for menu

--- components/intromenu.tsx
+++ components/intromenu.tsx
@@ -8,7 +8,7 @@ export default function IntroMenu() {
                 className={'text-center container flex items-center justify-center min-h-screen flex-col'}>
                 <div className={'-mt-[150px]'}>
                     <h1 className={'text-orange text-[54px] lg:text-[120px] tracking-widest'}>Menu</h1>
-                    <h2 className={'text-brown underline-yellow intro text-[28px] lg:text-[32px] tracking-widest'}>Lunch, diner &
+                    <h2 className={'text-brown underline-yellow intro text-[28px] lg:text-[32px] tracking-widest'}>Snack, diner &
                         wijnkaart</h2>
                 </div>
             </section>
